9c8a2fbe01fcca3408f6d7b53946989c8ecde88b
jinchaeeun <kbg05032@naver.com>
2021-03-08 15:22:40 +0900

    과제    1 Activity =>  APP 시작 화면 , 터치로 2번째 화면 전환    2 Activity => 10개 이미지 출력                       이미지 선택 시 3번째 화면 전환    3 Activity => 선택한 이미지 전체 화면 보기                       화면 터치 시 3번째 화면 닫기

--- app/src/main/java/com/example/exam_intent/SecondActivity.java
+++ app/src/main/java/com/example/exam_intent/SecondActivity.java
@@ -2,23 +2,91 @@ package com.example.exam_intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TableLayout;
+
+import java.io.ByteArrayOutputStream;
 
 public class SecondActivity extends AppCompatActivity {
-
+    private String TAG = "Seco_Act";
+    Bitmap sendBitmap;
+    TableLayout tableLayout;
+    ImageView iv;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
+        Log.i(TAG, "Create Second Activity");
+        //imageView = (ImageView)findViewById(R.id.imageView);
+
+        //tableLayout = (TableLayout) tableLayout.findViewById(R.id.secondLAY);
     }
 
-    public void onClick(View v){
-        Intent moveINT = new Intent(SecondActivity.this, ThirdActivity.class); //계속하면 화면이 쌓임.
+    public void ImageClick(View v){
+        //Intent 생성
+        Intent moveINT = new Intent(SecondActivity.this, ThirdActivity.class);
+        int clickValue = v.getId();
+        switch (clickValue){
+            case R.id.imageView1:
+                //moveINT.putExtra("idTxt","imageView1");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brush);
+                break;
+            case R.id.imageView2:
+                //moveINT.putExtra("idTxt","imageView2");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.frameillust);
+                break;
+            case R.id.imageView3:
+                //moveINT.putExtra("idTxt","imageView3");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image3);
+                break;
+            case R.id.imageView4:
+                //moveINT.putExtra("idTxt","imageView4");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image4);
+                break;
+            case R.id.imageView5:
+                //moveINT.putExtra("idTxt","imageView5");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image5);
+                break;
+            case R.id.imageView6:
+                //moveINT.putExtra("idTxt","imageView6");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.moon);
+                break;
+            case R.id.imageView7:
+                //moveINT.putExtra("idTxt","imageView7");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sky_prism);
+                break;
+            case R.id.imageView8:
+                //moveINT.putExtra("idTxt","imageView8");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rosesmall);
+                break;
+            case R.id.imageView9:
+                //moveINT.putExtra("idTxt","imageView9");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image9);
+                break;
+            case R.id.imageView10:
+                //moveINT.putExtra("idTxt","imageView10");
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image10);
+                break;
+        }
+
+        //putExtra 파일 크기 100kb이하인데 팅김 현상 발생.. bitmap으로 변형해서 넘김
+        //Bitmap sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brush);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        sendBitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] byteArray = stream.toByteArray();
+        moveINT.putExtra("image",byteArray);
+
+        //Activity 이동
         startActivity(moveINT);
-        // 현재 Activity  종료
-        // finish();
+
+        Log.e("ID", String.valueOf(v.getId()));
+
     }
 }
--- app/src/main/java/com/example/exam_intent/ThirdActivity.java
+++ app/src/main/java/com/example/exam_intent/ThirdActivity.java
@@ -3,19 +3,34 @@ package com.example.exam_intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    private Bitmap image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.third);
-    }
+        
+        //인텐트 받아오기
+        Intent moveINT = getIntent();
+        byte[] arr = getIntent().getByteArrayExtra("image");
+        
+        //Bitmap 이미지 출력
+        image = BitmapFactory.decodeByteArray(arr, 0, arr.length);
+        // imageview ui 요소 연결
+        ImageView iv = findViewById(R.id.imageView1);
+        // image 출력
+        iv.setImageBitmap(image);
 
+    }
+    //클릭 이벤트
     public void onClick(View v){
-        Intent moveINT = new Intent(ThirdActivity.this, SecondActivity.class);
-        startActivity(moveINT);
+        // 현재 Activity  종료
+        finish();
     }
 }
