b0280d64111d83a15eec35444c7a78abcf05af06
jinchaeeun <kbg05032@naver.com>
2021-03-09 10:58:46 +0900

    과제 완료 주의사항 이미지 크기 작은 것 써야함(튕김 현상 방지, 테이블레이아웃 사이즈 정렬)

--- app/src/main/java/com/example/exam_intent/SecondActivity.java
+++ app/src/main/java/com/example/exam_intent/SecondActivity.java
@@ -35,46 +35,37 @@ public class SecondActivity extends AppCompatActivity {
         int clickValue = v.getId();
         switch (clickValue){
             case R.id.imageView1:
-                //moveINT.putExtra("idTxt","imageView1");
-                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brush);
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image1);
                 break;
             case R.id.imageView2:
-                //moveINT.putExtra("idTxt","imageView2");
-                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.frameillust);
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image2);
                 break;
             case R.id.imageView3:
-                //moveINT.putExtra("idTxt","imageView3");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image3);
                 break;
             case R.id.imageView4:
-                //moveINT.putExtra("idTxt","imageView4");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image4);
                 break;
             case R.id.imageView5:
-                //moveINT.putExtra("idTxt","imageView5");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image5);
                 break;
             case R.id.imageView6:
-                //moveINT.putExtra("idTxt","imageView6");
-                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.moon);
+                sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image6);
                 break;
             case R.id.imageView7:
-                //moveINT.putExtra("idTxt","imageView7");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.sky_prism);
                 break;
             case R.id.imageView8:
-                //moveINT.putExtra("idTxt","imageView8");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.rosesmall);
                 break;
             case R.id.imageView9:
-                //moveINT.putExtra("idTxt","imageView9");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image9);
                 break;
             case R.id.imageView10:
-                //moveINT.putExtra("idTxt","imageView10");
                 sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.image10);
                 break;
         }
+        ////moveINT.putExtra("idTxt","imageView3");
 
         //putExtra 파일 크기 100kb이하인데 팅김 현상 발생.. bitmap으로 변형해서 넘김
         //Bitmap sendBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.brush);
